b7ad8b7c324e200a9d49c6926593aac1c6068b26
Husein <nvdhunter007@gmail.com>
2020-05-12 05:34:57 +0700

    add model properties

--- src/app/User.php
+++ src/app/User.php
@@ -2,13 +2,19 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    const VERIFIED_USER = '1';
+    const UNVERIFIED_USER = '0';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +22,12 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -25,15 +36,23 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function isVerified()
+    {
+        return $this->verified == USER::VERIFIED_USER;
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin == USER::ADMIN_USER;
+    }
+
+    public static function generateVerificationCode()
+    {
+        return Str::random(40);
+    }
 }
