97d68d2aa0e9f42b90c8fc0e4858934199e735cb
Husein <nvdhunter007@gmail.com>
2020-05-12 15:09:00 +0700

    add factories and seeder for all the models

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('verified')->default(User::UNVERIFIED_USER);
-            $table->string('verification_token');
+            $table->string('verification_token')->nullable();
             $table->string('admin')->default(User::REGULAR_USER);
             $table->timestamps();
         });
