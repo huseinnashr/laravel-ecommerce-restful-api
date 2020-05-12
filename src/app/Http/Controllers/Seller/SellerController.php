f25c4cdf412b20b26409bc3fef39fef9f7483ade
Husein <nvdhunter007@gmail.com>
2020-05-12 17:59:03 +0700

    implement seller method

--- src/app/Http/Controllers/Seller/SellerController.php
+++ src/app/Http/Controllers/Seller/SellerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Seller;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
@@ -14,28 +15,9 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $sellers = Seller::has('products')->get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return response()->json(['data' => $sellers], 200);
     }
 
     /**
@@ -46,40 +28,8 @@ class SellerController extends Controller
      */
     public function show($id)
     {
-        //
-    }
+        $seller = Seller::has('products')->findOrFail($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return response()->json(['data' => $seller], 200);
     }
 }
