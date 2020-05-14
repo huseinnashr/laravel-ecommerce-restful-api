b09d05347d92c8a84efbedf595eef302cad609b1
Husein <nvdhunter007@gmail.com>
2020-05-14 17:47:12 +0700

    hide pivot attr on category & product result

--- src/app/Category.php
+++ src/app/Category.php
@@ -14,6 +14,9 @@ class Category extends Model
         'name',
         'description',
     ];
+    protected $hidden = [
+        'pivot'
+    ];
 
     public function products()
     {
--- src/app/Product.php
+++ src/app/Product.php
@@ -21,6 +21,9 @@ class Product extends Model
         'image',
         'seller_id',
     ];
+    protected $hidden = [
+        'pivot'
+    ];
 
     public function isAvailable()
     {
