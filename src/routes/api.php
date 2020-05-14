0d5f8869ed5f049004256bb59995be6394c8748f
Husein <nvdhunter007@gmail.com>
2020-05-14 15:34:32 +0700

    implement buyer complex operation

--- src/routes/api.php
+++ src/routes/api.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
  * Buyers
  */
 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' => ['index']]);
+Route::resource('buyers.sellers', 'Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.products', 'Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.transactions', 'Buyer\BuyerTransactionController', ['only' => ['index']]);
 
 /**
  * Categories
