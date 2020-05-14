760787292e7a5dd881f88c182d6af63b3fdcb1af
Husein <nvdhunter007@gmail.com>
2020-05-14 16:35:25 +0700

    fix relation to 1 product to m transaction

--- src/app/Product.php
+++ src/app/Product.php
@@ -34,7 +34,7 @@ class Product extends Model
 
     public function transactions()
     {
-        return $this->belongsToMany(Transaction::class);
+        return $this->hasMany(Transaction::class);
     }
 
     public function categories()
